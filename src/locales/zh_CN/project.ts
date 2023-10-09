dc859ce8d4ec9def4c57ef001722c8e39a2127e0
xt <759928840@qq.com>
2023-10-09 09:55:54 +0800

    新增库名提示

--- src/locales/zh_CN/project.ts
+++ src/locales/zh_CN/project.ts
@@ -493,6 +493,8 @@ export default {
     datax_json_template: 'JSON',
     datax_target_datasource_type: '目标源类型',
     datax_target_database: '目标库',
+    datax_ora_tips: 'oracle库名',
+    datax_sql_tips: 'sqlserver模式名，默认dbo',
     datax_target_table: '目标表',
     datax_source_table: '来源表',
     datax_source_database: '来源库',
