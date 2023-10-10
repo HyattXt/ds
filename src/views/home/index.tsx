b633fee69166a133a35b6023aac4e2dcff16625f
xt <759928840@qq.com>
2023-10-10 16:10:40 +0800

    修改统一平台嵌入逻辑

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -61,7 +61,6 @@ export default defineComponent({
       initData()
 
     }
-    loginNew();
     const { t, locale } = useI18n()
     const dateRef = ref([getTime(startOfToday()), Date.now()])
     const taskStateRef = ref()
@@ -79,6 +78,7 @@ export default defineComponent({
     }
 
     onMounted(() => {
+      loginNew();
       if (userStore.sessionId){
         initData()
       }
