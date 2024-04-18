2a14096a300108591a451452c34c6a53e934273a
xt <759928840@qq.com>
2024-04-18 13:51:17 +0800

    修改cue公用组件

---
+++ src/components/cue/crud-form.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-config-provider :locale="zhCn">
+    <div class="cue-drag-layout flex-row">
+      <slot name="tree"></slot>
+      <div class="cue-drag-layout__mainview" :style="{width: width}">
+        <div class="cue-crud cue-crud-v2">
+          <slot name="header"></slot>
+          <div class="crud-v2-condition" >
+            <div class="cue-crud__header-condition">
+              <div class="cue-crud__header-content">
+                <slot name="condition"></slot>
+                <slot name="query"></slot>
+              </div>
+            </div>
+          </div>
+          <div class="cue-crud__body">
+            <slot name="table"></slot>
+          </div>
+          <div class="cue-crud__footer">
+            <div class="cue-crud__footer-tab">
+            </div>
+            <div class="cue-crud__footer-pager">
+              <slot name="page"></slot>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </el-config-provider>
+</template>
+
+<style scoped>
+
+</style>
+<script setup>
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+
+const props = defineProps({
+   width: {
+    type: String,
+    default: '100%',
+  },
+})
+</script>
---
+++ src/components/cue/crud-page.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-config-provider :locale="zhCn">
+    <el-pagination
+        background
+        :default-page-size="30"
+        :page-sizes="[30, 90, 180, 300]"
+        layout="total, sizes, prev, pager, next"
+        :total="paginationReactive.itemCount"
+        @change="handlePageChange"
+        popper-class="page-select"
+    >
+    </el-pagination>
+  </el-config-provider>
+</template>
+
+<script setup>
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+
+const props = defineProps({
+  paginationReactive: Object
+})
+const emit = defineEmits(['page-change'])
+
+function handlePageChange(page, pageSize) {
+  emit('page-change',page, pageSize)
+}
+</script>
+
+<style scoped>
+
+</style>
