f197d4b08706d577f82c2ad0bb472781f99c1a41
xt <759928840@qq.com>
2024-07-03 16:40:38 +0800

    暂时恢复上标题

--- src/views/data-labels/index.vue
+++ src/views/data-labels/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="label-back">
+    <CrudHead title="用户画像" style="border-bottom: 1px solid rgb(239, 239, 245)"/>
     <n-grid :x-gap="12" :y-gap="8" :cols="9">
       <n-grid-item span="2">
         <div>
