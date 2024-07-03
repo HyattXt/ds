14fa281be52eafcbd58e247f42310652143a2c09
xt <759928840@qq.com>
2024-07-03 16:15:36 +0800

    增加结果列排序

--- src/views/projects/workflow/components/task/sql-box.vue
+++ src/views/projects/workflow/components/task/sql-box.vue
@@ -51,7 +51,7 @@
               <div style="height: 100%">
                 <el-table :data="item" border show-overflow-tooltip	height="100%">
                   <el-table-column type="index" label="#"/>
-                  <el-table-column v-for="key in Object.keys(item[0] || {})" :prop="key" :label="key"/>
+                  <el-table-column v-for="key in Object.keys(item[0] || {})" :prop="key" :label="key" sortable />
                 </el-table>
               </div>
             </n-tab-pane>
